35c4f1cd93f520adff4ae99dec14021d42f930ba
SRasoul <shahinrasoul@gmail.com>
2024-10-07 21:26:28 +0100

    Added TicketServiceImplTest class

---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,26 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.Test;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TicketServiceImplTest {
+
+	@Test
+	public void testAllTicketTypeRequest(){
+		TicketTypeRequest  adultTicket = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
+		TicketTypeRequest  childTicket = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 3);
+		TicketTypeRequest infantTicket = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1);
+		
+		assertEquals(TicketTypeRequest.Type.ADULT, adultTicket.getTicketType(), "Ticket type should be ADULT");
+		assertEquals(2, adultTicket.getNoOfTickets(), "Number of tickets should be 2");
+		
+		assertEquals(TicketTypeRequest.Type.CHILD, childTicket.getTicketType(), "Ticket type should be CHILD");
+		assertEquals(3, childTicket.getNoOfTickets(), "Number of tickets should be 3");
+		
+		assertEquals(TicketTypeRequest.Type.INFANT, infantTicket.getTicketType(), "Ticket type should be INFANT");
+		assertEquals(1, infantTicket.getNoOfTickets(), "Number of tickets should be 1");
+		
+	}
+}
