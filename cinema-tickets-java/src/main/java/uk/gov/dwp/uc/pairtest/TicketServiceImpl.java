4aa27282906048d8a95a1722bb2f462e9acae2a4
SRasoul <shahinrasoul@gmail.com>
2024-10-08 01:46:17 +0100

    Implement amount calculation.

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -24,13 +24,14 @@ public class TicketServiceImpl implements TicketService {
         
         validateAccountAndRequest(accountId, ticketTypeRequests);
         
-        int totalAmount = 50;
+        int totalAmount = 0;
         int totalSeatsToReserve = 0;
         boolean hasAdult = false;
         
         
         for(TicketTypeRequest request : ticketTypeRequests) {
             totalSeatsToReserve += calculateSeatsForRequest(request);
+            totalAmount += calculateRequestTotalAmount(request);
             
             if(request.getTicketType() == TicketTypeRequest.Type.ADULT){
                 hasAdult = true;
@@ -43,6 +44,20 @@ public class TicketServiceImpl implements TicketService {
         ticketPaymentService.makePayment(accountId, totalAmount);
         seatReservationService.reserveSeat(accountId, totalSeatsToReserve);
         
+    }
+    
+    private int calculateRequestTotalAmount(TicketTypeRequest request) {
+        int ticket = request.getNoOfTickets();
+        switch (request.getTicketType()){
+            case ADULT:
+                return ticket * 25;
+            case CHILD:
+                return ticket * 15;
+            case INFANT:
+                return 0;
+            default:
+                throw new IllegalArgumentException("Invalid type request");
+        }
     }
     
     private int calculateSeatsForRequest(TicketTypeRequest request) {
